e07cfa7f91eed1946d3412648d9d05ce6f0f81b2
cuibowen <cuibowen@xiaomi.com>
2019-01-21 10:35:58 +0800

    Spark SQL,Datasets and DataFrames test

---
+++ spark-learning/src/test/java/SQLTest.java
@@ -0,0 +1,212 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.*;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.apache.spark.sql.functions.col;
+
+/**
+ * @description:
+ * @author: cuibowen
+ * @create: 2019/01/18
+ **/
+public class SQLTest {
+
+    static SparkSession spark;
+
+    @BeforeClass
+    public static void beforeClass() {
+        spark = SparkSession
+                .builder()
+                .appName("Java Spark SQL example")
+                .master("local[*]")
+                //.config("spark.some.config.option", "some-value")
+                .getOrCreate();
+    }
+
+    @Test
+    public void testJson() throws AnalysisException {
+        Dataset<Row> df = spark.read().json("src/main/resources/people.json");
+
+        // Displays the content of the DataFrame to stdout
+        df.show();
+        // +----+-------+
+        // | age|   name|
+        // +----+-------+
+        // |null|Michael|
+        // |  30|   Andy|
+        // |  19| Justin|
+        // +----+-------+
+        // $example off:create_df$
+
+        Assert.assertTrue(df.count() == 3);
+
+        // $example on:untyped_ops$
+        // Print the schema in a tree format
+        df.printSchema();
+        // root
+        // |-- age: long (nullable = true)
+        // |-- name: string (nullable = true)
+
+        Assert.assertTrue(df.columns().length == 2);
+        // Select only the "name" column
+        df.select("name").show();
+        // +-------+
+        // |   name|
+        // +-------+
+        // |Michael|
+        // |   Andy|
+        // | Justin|
+        // +-------+
+
+        // Select everybody, but increment the age by 1
+        df.select(col("name"), col("age").plus(1)).show();
+        // +-------+---------+
+        // |   name|(age + 1)|
+        // +-------+---------+
+        // |Michael|     null|
+        // |   Andy|       31|
+        // | Justin|       20|
+        // +-------+---------+
+
+        // Select people older than 21
+        df.filter(col("age").gt(21)).show();
+        // +---+----+
+        // |age|name|
+        // +---+----+
+        // | 30|Andy|
+        // +---+----+
+
+        // Count people by age
+        df.groupBy("age").count().show();
+        // +----+-----+
+        // | age|count|
+        // +----+-----+
+        // |  19|    1|
+        // |null|    1|
+        // |  30|    1|
+        // +----+-----+
+        // $example off:untyped_ops$
+
+        // $example on:run_sql$
+        // Register the DataFrame as a SQL temporary view
+        df.createOrReplaceTempView("people");
+
+        Dataset<Row> sqlDF = spark.sql("SELECT * FROM people");
+        sqlDF.show();
+        // +----+-------+
+        // | age|   name|
+        // +----+-------+
+        // |null|Michael|
+        // |  30|   Andy|
+        // |  19| Justin|
+        // +----+-------+
+        // $example off:run_sql$
+
+        // $example on:global_temp_view$
+        // Register the DataFrame as a global temporary view
+        df.createGlobalTempView("people");
+
+        // Global temporary view is tied to a system preserved database `global_temp`
+        spark.sql("SELECT * FROM global_temp.people").show();
+        // +----+-------+
+        // | age|   name|
+        // +----+-------+
+        // |null|Michael|
+        // |  30|   Andy|
+        // |  19| Justin|
+        // +----+-------+
+
+        // Global temporary view is cross-session
+        spark.newSession().sql("SELECT * FROM global_temp.people").show();
+        // +----+-------+
+        // | age|   name|
+        // +----+-------+
+        // |null|Michael|
+        // |  30|   Andy|
+        // |  19| Justin|
+        // +----+-------+
+        // $example off:global_temp_view$
+    }
+
+    public static class Person {
+        private String name;
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+
+    @Test
+    public void testBean() {
+        Person person = new Person();
+        person.setName("Andy");
+        person.setAge(32);
+
+        // Encoders are created for Java beans
+        Encoder<Person> personEncoder = Encoders.bean(Person.class);
+        Dataset<Person> javaBeanDS = spark.createDataset(
+                Collections.singletonList(person),
+                personEncoder
+        );
+        javaBeanDS.show();
+        // +---+----+
+        // |age|name|
+        // +---+----+
+        // | 32|Andy|
+        // +---+----+
+
+        Assert.assertEquals(javaBeanDS.first().getName(), "Andy");
+
+        String path = "src/main/resources/people.json";
+        Dataset<Person> personDS = spark.read().json(path).as(personEncoder);
+        personDS.show();
+        // +----+-------+
+        // | age|   name|
+        // +----+-------+
+        // |null|Michael|
+        // |  30|   Andy|
+        // |  19| Justin|
+        // +----+-------+
+        Assert.assertFalse(personDS.isEmpty());
+    }
+
+    @Test
+    public void testRDD() {
+        // Create an RDD of Person objects from a text file
+        JavaRDD<Person> personJavaRDD = spark.read()
+                .textFile("src/main/resources/people.txt")
+                .javaRDD()
+                .map(line -> {
+                    String[] parts = line.split(",");
+                    Person person = new Person();
+                    person.setName(parts[0]);
+                    person.setAge(Integer.parseInt(parts[1].trim()));
+                    return person;
+                });
+
+        // Apply a schema to an RDD of JavaBeans to get a DataFrame
+        Dataset<Row> personDF = spark.createDataFrame(personJavaRDD, Person.class);
+        personDF.printSchema();
+        Assert.assertFalse(personDF.isEmpty());
+    }
+}
